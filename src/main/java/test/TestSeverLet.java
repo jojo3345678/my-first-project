34f86ea2ad9ac2bd1e1a9c89df3da479e68d97b1
Yo <yoyo79791212@gmail.com>
2023-04-20 11:52:47 +0800

    Update  dog ->dog!

--- src/main/java/test/TestSeverLet.java
+++ src/main/java/test/TestSeverLet.java
@@ -13,6 +13,6 @@ public class TestSeverLet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("dog");
+		response.getWriter().append("dog!");
 	}
 }
